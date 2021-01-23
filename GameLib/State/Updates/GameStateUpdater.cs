5f15b397ccbdc7d4c8f05fe6899ffa2015d33ec7
Christopher Kyle Horton <christhehorton@gmail.com>
2021-01-23 09:45:16 -0500

    Move round update functions to new class

--- GameLib/State/Updates/GameStateUpdater.cs
+++ GameLib/State/Updates/GameStateUpdater.cs
@@ -30,7 +30,7 @@ namespace WMD.Game.State.Updates
             };
 
             return gameState.CurrentPlayerIndex == 0
-                ? AdvanceToNextRound(gameState)
+                ? GameStateRoundAdvancer.AdvanceToNextRound(gameState)
                 : (gameState, null);
         }
 
@@ -219,47 +219,6 @@ namespace WMD.Game.State.Updates
             return gameState with { Players = CreatePlayerListCopyWithUpdatedStateForPlayer(gameState.Players, playerIndex, playerState) };
         }
 
-        private static (GameState, RoundUpdateResult) AdvanceToNextRound(GameState gameState)
-        {
-            var result = CreateRoundUpdateResult(gameState);
-            var updatedGameState = ApplyRoundUpdates(gameState, result);
-            updatedGameState = updatedGameState with { CurrentRound = updatedGameState.CurrentRound + 1 };
-
-            return (updatedGameState, result);
-        }
-
-        private static GameState ApplyRoundUpdates(GameState gameState, RoundUpdateResult roundUpdates)
-        {
-            var updatedGameState = gameState;
-
-            try
-            {
-                foreach (var roundUpdate in roundUpdates.Items)
-                {
-                    updatedGameState = ApplyRoundUpdateItem(updatedGameState, roundUpdate);
-                }
-            }
-            catch (ArgumentException ex)
-            {
-                throw new ArgumentException("The provided round update result contained an invalid item.", nameof(roundUpdates), ex);
-            }
-
-            return updatedGameState;
-        }
-
-        private static GameState ApplyRoundUpdateItem(GameState gameState, RoundUpdateResultItem roundUpdate)
-        {
-            switch (roundUpdate)
-            {
-                case PlayerHenchmenPaid playerHenchmenPaid:
-                    return AdjustMoneyForPlayer(gameState, playerHenchmenPaid.PlayerIndex, -1 * playerHenchmenPaid.TotalPaidAmount);
-                case PlayerHenchmenQuit playerHenchmenQuit:
-                    return AdjustHenchmenForPlayer(gameState, playerHenchmenQuit.PlayerIndex, -1 * playerHenchmenQuit.NumberOfHenchmenQuit);
-                default:
-                    throw new ArgumentException($"Unrecognized {typeof(RoundUpdateResultItem).Name} subclass: {roundUpdate.GetType().Name}.");
-            }
-        }
-
         private static IReadOnlyList<Player> CreatePlayerListCopyWithUpdatedStateForPlayer(IReadOnlyList<Player> players, int playerIndex, PlayerState state)
         {
             var updatedPlayers = new Queue<Player>(players.Count);
@@ -270,23 +229,6 @@ namespace WMD.Game.State.Updates
             }
 
             return updatedPlayers.ToList().AsReadOnly();
-        }
-
-        private static RoundUpdateResult CreateRoundUpdateResult(GameState gameState)
-        {
-            var henchmenPayments = Enumerable.Range(0, gameState.Players.Count)
-                .Where(index => gameState.Players[index].State.WorkforceState.NumberOfHenchmen > 0)
-                .Select(index => new PlayerHenchmenPaid(gameState, index));
-
-            var henchmenQuittings = Enumerable.Range(0, gameState.Players.Count)
-                .Where(index => gameState.Players[index].State.Money <= 0 && gameState.Players[index].State.WorkforceState.NumberOfHenchmen > 0)
-                .Select(index => new PlayerHenchmenQuit(index, gameState.Players[index].State.WorkforceState.NumberOfHenchmen));
-
-            var allUpdates = new List<RoundUpdateResultItem>()
-                .Concat(henchmenPayments)
-                .Concat(henchmenQuittings);
-
-            return new RoundUpdateResult(gameState, gameState.CurrentRound, allUpdates);
         }
     }
 }
