4b6e24011b9a39de796cea26120854ae0c280102
Christopher Kyle Horton <christhehorton@gmail.com>
2021-02-01 20:10:55 -0500

    Add unit test project

---
+++ wmd-core-test/State/Data/Henchmen/WorkforceStateTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WMD.Game.State.Data.Henchmen;
+
+namespace WMD.Game.Test.State.Data.Henchmen
+{
+    [TestClass]
+    public class WorkforceStateTests
+    {
+        [TestMethod]
+        public void DefaultConstructor_ShouldCreateInstanceWithExpectedDefaultValues()
+        {
+            var subject = new WorkforceState();
+
+            Assert.AreEqual(0, subject.NumberOfHenchmen);
+            Assert.AreEqual(7, subject.DailyPayRate);
+        }
+    }
+}
