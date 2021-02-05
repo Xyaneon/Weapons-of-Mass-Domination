21a726db993672cda86a145703ff46cb9d7d702b
Christopher Kyle Horton <christhehorton@gmail.com>
2021-02-04 21:12:35 -0500

    Add PlayerState unit tests

---
+++ wmd-core-test/State/Data/Players/PlayerStateTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WMD.Game.State.Data.Henchmen;
+using WMD.Game.State.Data.Players;
+using WMD.Game.State.Data.Research;
+
+namespace WMD.Game.Test.State.Data.Players
+{
+    [TestClass]
+    public class PlayerStateTests
+    {
+        [TestMethod]
+        public void DefaultConstructor_ShouldSetPropertiesToExpectedValues()
+        {
+            var expectedResearchState = new ResearchState();
+            var expectedWorkforceState = new WorkforceState();
+
+            var actual = new PlayerState();
+
+            Assert.IsFalse(actual.HasResigned);
+            Assert.AreEqual(0, actual.Land);
+            Assert.AreEqual(0, actual.Money);
+            Assert.AreEqual(expectedResearchState, actual.ResearchState);
+            Assert.IsNull(actual.SecretBase);
+            Assert.AreEqual(expectedWorkforceState, actual.WorkforceState);
+        }
+    }
+}
