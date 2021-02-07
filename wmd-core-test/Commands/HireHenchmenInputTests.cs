1220473e19f6eb5902dc74568a4563fe92c25bf7
Christopher Kyle Horton <christhehorton@gmail.com>
2021-02-07 11:45:03 -0500

    Add unit tests

---
+++ wmd-core-test/Commands/HireHenchmenInputTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WMD.Game.Commands;
+
+namespace WMD.Game.Test.Commands
+{
+    [TestClass]
+    public class HireHenchmenInputTests
+    {
+        [TestMethod]
+        public void OpenPositionsOffered_ShouldThrowIfProvidedValueIsLessThanOne()
+        {
+            var actual = Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            {
+                _ = new HireHenchmenInput() with { OpenPositionsOffered = 0 };
+            });
+
+            Assert.IsTrue(actual.Message.Contains("The number of open positions offered must be greater than zero."));
+        }
+    }
+}
---
+++ wmd-core-test/Commands/PurchaseUnclaimedLandInputTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WMD.Game.Commands;
+
+namespace WMD.Game.Test.Commands
+{
+    [TestClass]
+    public class PurchaseUnclaimedLandInputTests
+    {
+        [TestMethod]
+        public void AreaToPurchase_ShouldThrowIfProvidedValueIsLessThanZero()
+        {
+            var actual = Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            {
+                _ = new PurchaseUnclaimedLandInput() with { AreaToPurchase = -1 };
+            });
+
+            Assert.IsTrue(actual.Message.Contains("The area of land to purchase cannot be negative."));
+        }
+    }
+}
