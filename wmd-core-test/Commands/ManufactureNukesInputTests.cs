b9ab87052778fb4478384f6cf95c8f6904815f28
Christopher Kyle Horton <christhehorton@gmail.com>
2021-02-27 10:37:46 -0500

    Add ManufactureNukesInput

---
+++ wmd-core-test/Commands/ManufactureNukesInputTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WMD.Game.Commands;
+
+namespace WMD.Game.Test.Commands
+{
+    [TestClass]
+    public class ManufactureNukesInputTests
+    {
+        [TestMethod]
+        public void NumberOfNukesToManufacture_ShouldThrowIfProvidedValueIsLessThanOne()
+        {
+            var actual = Assert.ThrowsException<ArgumentOutOfRangeException>(() =>
+            {
+                _ = new ManufactureNukesInput() with { NumberOfNukesToManufacture = 0 };
+            });
+
+            Assert.IsTrue(actual.Message.Contains("The number of nukes to manufacture must be greater than zero."));
+        }
+    }
+}
---
+++ wmd-core/Commands/ManufactureNukesInput.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WMD.Game.Commands
+{
+    /// <summary>
+    /// Additional input data for the manufacture nukes action.
+    /// </summary>
+    public record ManufactureNukesInput : CommandInput
+    {
+        /// <summary>
+        /// Gets or initializes the number of nukes to manufacture.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// The provided value is less than one.
+        /// </exception>
+        public int NumberOfNukesToManufacture
+        {
+            get => _numberOfNukesToManufacture;
+            init
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, "The number of nukes to manufacture must be greater than zero.");
+                }
+                _numberOfNukesToManufacture = value;
+            }
+        }
+
+        private int _numberOfNukesToManufacture;
+    }
+}
