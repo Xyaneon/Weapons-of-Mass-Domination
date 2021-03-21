ccab40c242aeb5ea50b855f34bf5f43354ec780f
Christopher Kyle Horton <christhehorton@gmail.com>
2021-03-21 10:49:06 -0400

    Disable land purchasing when the player would be unable to buy any

--- src/wmd-core/Commands/PurchaseUnclaimedLandCommand.cs
+++ src/wmd-core/Commands/PurchaseUnclaimedLandCommand.cs
@@ -11,28 +11,37 @@ namespace WMD.Game.Commands
     /// </summary>
     public class PurchaseUnclaimedLandCommand : GameCommand<PurchaseUnclaimedLandInput, PurchaseUnclaimedLandResult>
     {
+        private const string InvalidOperationException_PlayerCannotPurchaseAnyLand = "The current player is unable to purchase any land.";
+        private const string InvalidOperationException_PlayerCannotPurchaseRequestedAmountOfLand = "The current player does not have enough money to purchase the requested amount of land.";
+        private const string InvalidOperationException_PlayerRequestingToPurchaseMoreLandThanIsAvailable = "There is not enough unclaimed land left to satisfy the current player's requested amount to purchase.";
+
         public override bool CanExecuteForState([DisallowNull] GameState gameState)
         {
-            return true;
+            return GameStateChecks.CurrentPlayerCouldPurchaseLand(gameState);
         }
 
         public override bool CanExecuteForStateAndInput([DisallowNull] GameState gameState, [DisallowNull] PurchaseUnclaimedLandInput input)
         {
-            return !(CurrentPlayerHasInsufficientFunds(gameState, input) || NotEnoughLandToSatisfyPurchaseAmount(gameState, input));
+            return CanExecuteForState(gameState) && (!(CurrentPlayerHasInsufficientFunds(gameState, input) || NotEnoughLandToSatisfyPurchaseAmount(gameState, input)));
         }
 
         public override PurchaseUnclaimedLandResult Execute([DisallowNull] GameState gameState, [DisallowNull] PurchaseUnclaimedLandInput input)
         {
+            if (!GameStateChecks.CurrentPlayerCouldPurchaseLand(gameState))
+            {
+                throw new InvalidOperationException(InvalidOperationException_PlayerCannotPurchaseAnyLand);
+            }
+
             decimal totalPurchasePrice = LandAreaCalculator.CalculateTotalPurchasePrice(gameState, input.AreaToPurchase);
 
             if (CurrentPlayerHasInsufficientFunds(gameState, input))
             {
-                throw new InvalidOperationException("The current player does not have enough money to purchase the requested amount of land.");
+                throw new InvalidOperationException(InvalidOperationException_PlayerCannotPurchaseRequestedAmountOfLand);
             }
 
             if (NotEnoughLandToSatisfyPurchaseAmount(gameState, input))
             {
-                throw new InvalidOperationException("There is not enough unclaimed land left to satisfy the current player's requested amount to purchase.");
+                throw new InvalidOperationException(InvalidOperationException_PlayerRequestingToPurchaseMoreLandThanIsAvailable);
             }
 
             GameState updatedGameState = GameStateUpdater.GiveUnclaimedLandToPlayer(gameState, gameState.CurrentPlayerIndex, input.AreaToPurchase);
