d103cf9f6310d87671fb2152bd8c626653f1e433
Christopher Kyle Horton <christhehorton@gmail.com>
2021-03-27 12:55:03 -0400

    Add ReputationDecay

---
+++ test/wmd-core-test/State/Updates/Rounds/ReputationDecayTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WMD.Game.State.Updates.Rounds;
+
+namespace WMD.Game.Test.State.Updates.Rounds
+{
+    [TestClass]
+    public class ReputationDecayTests
+    {
+        [TestMethod]
+        public void Constructor_ShouldCreateObjectWithGivenValues()
+        {
+            int expectedPlayerIndex = 2;
+            int expectedReputationPercentageLost = 3;
+
+            var actualRecord = new ReputationDecay(expectedPlayerIndex, expectedReputationPercentageLost);
+
+            Assert.AreEqual(expectedPlayerIndex, actualRecord.PlayerIndex);
+            Assert.AreEqual(expectedReputationPercentageLost, actualRecord.ReputationPercentageLost);
+        }
+    }
+}
