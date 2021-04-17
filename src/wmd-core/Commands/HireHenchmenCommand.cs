dd140e581987ddec5439747fee0e8edcdc36ec68
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-17 12:14:09 -0400

    Make having money a requirement for hiring henchmen

--- src/wmd-core/Commands/HireHenchmenCommand.cs
+++ src/wmd-core/Commands/HireHenchmenCommand.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics.CodeAnalysis;
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
 using WMD.Game.State.Data;
 using WMD.Game.State.Updates;
 using WMD.Game.State.Utility;
@@ -10,18 +11,25 @@ namespace WMD.Game.Commands
     /// </summary>
     public class HireHenchmenCommand : GameCommand<HireHenchmenInput, HireHenchmenResult>
     {
+        private const string InvalidOperationException_NotEnoughMoney = "The current player does not have enough money to hire henchmen.";
+
         public override bool CanExecuteForState([DisallowNull] GameState gameState)
         {
-            return true;
+            return !GameStateChecks.CurrentPlayerHasNoMoney(gameState);
         }
 
         public override bool CanExecuteForStateAndInput([DisallowNull] GameState gameState, [DisallowNull] HireHenchmenInput input)
         {
-            return true;
+            return CanExecuteForState(gameState);
         }
 
         public override HireHenchmenResult Execute([DisallowNull] GameState gameState, [DisallowNull] HireHenchmenInput input)
         {
+            if (GameStateChecks.CurrentPlayerHasNoMoney(gameState))
+            {
+                throw new InvalidOperationException(InvalidOperationException_NotEnoughMoney);
+            }
+
             int henchmenHired = HenchmenCalculator.CalculateNumberOfHenchmenHired(gameState, input.OpenPositionsOffered);
             GameState updatedGameState = GameStateUpdater.AdjustHenchmenForPlayer(gameState, gameState.CurrentPlayerIndex, henchmenHired);
 
