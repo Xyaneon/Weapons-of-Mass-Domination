b269a32fa2aed410d92bca1a8a7f43aacab34072
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-24 11:30:45 -0400

    Move MinimumDailyWage to a new HenchmenConstants class

---
+++ src/wmd-core/Constants/HenchmenConstants.cs
@@ -0,0 +1,21 @@
+﻿namespace WMD.Game.Constants
+{
+    /// <summary>
+    /// Provides constants related to henchmen.
+    /// </summary>
+    public static class HenchmenConstants
+    {
+        /// <summary>
+        /// The minimum daily wage.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The <see cref="State.Data.Henchmen.WorkforceState.DailyPayRate"/> property is initialized to this value by default.
+        /// </para>
+        /// <para>
+        /// If a player chooses to pay less than this amount, then their henchmen will be much more likely to quit.
+        /// </para>
+        /// </remarks>
+        public const decimal MinimumDailyWage = 7;
+    }
+}
