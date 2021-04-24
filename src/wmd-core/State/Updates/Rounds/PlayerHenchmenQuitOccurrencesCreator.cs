38122b3cb41beff1c85ab0de0ab47723a5423a85
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-24 13:05:08 -0400

    Add underpaid henchmen quitting calculation

--- src/wmd-core/State/Updates/Rounds/PlayerHenchmenQuitOccurrencesCreator.cs
+++ src/wmd-core/State/Updates/Rounds/PlayerHenchmenQuitOccurrencesCreator.cs
@@ -1,6 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using WMD.Game.Constants;
 using WMD.Game.State.Data;
+using WMD.Game.State.Data.Henchmen;
 using WMD.Game.State.Data.Players;
 
 namespace WMD.Game.State.Updates.Rounds
@@ -9,13 +12,37 @@ namespace WMD.Game.State.Updates.Rounds
     {
         public override IEnumerable<RoundUpdateResultItem> CreateOccurrences(GameState gameState) =>
             CreateRangeOfPlayerIndices(gameState)
-                .Where(index => PlayerCannotPayTheirHenchmen(gameState.Players[index].State))
+                .Where(index => PlayerCannotPayTheirHenchmen(gameState.Players[index].State) || PlayerHenchmenAreUnderpaid(gameState.Players[index].State))
                 .Select(index => CreatePlayerHenchmenQuitOccurrence(gameState, index));
 
-        private static PlayerHenchmenQuit CreatePlayerHenchmenQuitOccurrence(GameState gameState, int index) =>
-            new(index, gameState.Players[index].State.WorkforceState.NumberOfHenchmen);
+        private static PlayerHenchmenQuit CreatePlayerHenchmenQuitOccurrence(GameState gameState, int index)
+        {
+            PlayerState playerState = gameState.Players[index].State;
+
+            if (PlayerCannotPayTheirHenchmen(playerState))
+            {
+                return new(index, playerState.WorkforceState.NumberOfHenchmen);
+            }
+
+            if (PlayerHenchmenAreUnderpaid(playerState))
+            {
+                int henchmenQuit = CalculateHenchmenQuitDueToUnderpay(playerState.WorkforceState);
+                return new(index, henchmenQuit);
+            }
+
+            throw new InvalidOperationException($"Conditions not met for player's henchmen to quit (index={index}).");
+        }
 
         private static bool PlayerCannotPayTheirHenchmen(PlayerState playerState) =>
             playerState.Money <= 0 && playerState.WorkforceState.NumberOfHenchmen > 0;
+
+        private static bool PlayerHenchmenAreUnderpaid(PlayerState playerState) =>
+            playerState.WorkforceState.DailyPayRate < HenchmenConstants.MinimumDailyWage && playerState.WorkforceState.NumberOfHenchmen > 0;
+
+        private static int CalculateHenchmenQuitDueToUnderpay(WorkforceState workforceState)
+        {
+            double underpayPercentage = 1 - (double)workforceState.DailyPayRate / (double)HenchmenConstants.MinimumDailyWage;
+            return (int)Math.Ceiling(workforceState.NumberOfHenchmen * underpayPercentage);
+        }
     }
 }
