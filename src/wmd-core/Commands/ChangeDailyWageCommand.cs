74ef23595f1d078855158f6e8899d7ed55ba711d
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-24 21:55:52 -0400

    Add ChangeDailyWageCommand class

---
+++ src/wmd-core/Commands/ChangeDailyWageCommand.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using WMD.Game.State.Data;
+using WMD.Game.State.Updates;
+
+namespace WMD.Game.Commands
+{
+    /// <summary>
+    /// The command for the current player changing their daily wage for their henchmen.
+    /// </summary>
+    public class ChangeDailyWageCommand : GameCommand<ChangeDailyWageInput, ChangeDailyWageResult>
+    {
+        public override bool CanExecuteForState([DisallowNull] GameState gameState) => true;
+
+        public override bool CanExecuteForStateAndInput([DisallowNull] GameState gameState, ChangeDailyWageInput input) =>
+            CanExecuteForState(gameState);
+
+        public override ChangeDailyWageResult Execute([DisallowNull] GameState gameState, [DisallowNull] ChangeDailyWageInput input)
+        {
+            decimal oldDailyWage = gameState.CurrentPlayer.State.WorkforceState.DailyPayRate;
+            GameState updatedGameState = GameStateUpdater.SetDailyWageForPlayer(gameState, gameState.CurrentPlayerIndex, input.NewDailyWage);
+
+            return new ChangeDailyWageResult(updatedGameState, gameState.CurrentPlayerIndex, oldDailyWage, input.NewDailyWage);
+        }
+    }
+}
