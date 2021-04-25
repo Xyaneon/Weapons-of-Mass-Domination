cf724a88350577801cc0ef517b8142f5231bcc3d
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-25 10:35:35 -0400

    Remove upper limit display on change wage input prompt

--- src/wmd-console/UI/Commands/ChangeDailyWageInputRetriever.cs
+++ src/wmd-console/UI/Commands/ChangeDailyWageInputRetriever.cs
@@ -9,13 +9,13 @@ namespace WMD.Console.UI.Commands
     class ChangeDailyWageInputRetriever : ICommandInputRetriever
     {
         private const string ConfirmationPromptFormatString = "Your henchmen's new daily wage will be {0:C}. You currently have {1:N0} henchmen, meaning you will spend {2:C} total each day on labor costs. Confirm?";
-        private const string GetNewDailyWagePromptFormatString = "You currently pay your henchmen {0:C} each every day. Please enter the new amount ({1:C} to {2:C})";
+        private const string GetNewDailyWagePromptFormatString = "You currently pay your henchmen {0:C} each every day. Please enter the new amount ({1:C} or above)";
 
         public CommandInput? GetCommandInput(GameState gameState)
         {
             decimal currentDailyWage = gameState.CurrentPlayer.State.WorkforceState.DailyPayRate;
             var allowedSpendingAmounts = new DecimalRange(0.0M, decimal.MaxValue);
-            var prompt = string.Format(GetNewDailyWagePromptFormatString, currentDailyWage, allowedSpendingAmounts.Minimum, allowedSpendingAmounts.Maximum);
+            var prompt = string.Format(GetNewDailyWagePromptFormatString, currentDailyWage, allowedSpendingAmounts.Minimum);
 
             var newDailyWage = UserInput.GetDecimal(prompt, allowedSpendingAmounts);
             if (newDailyWage < 0.0M)
