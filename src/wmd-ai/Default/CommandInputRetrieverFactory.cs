70ccac2ec83d9ca5e29cf17350dc8faa51f8c62a
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-25 10:31:57 -0400

    Add change daily wage support to AI

--- src/wmd-ai/Default/CommandInputRetrieverFactory.cs
+++ src/wmd-ai/Default/CommandInputRetrieverFactory.cs
@@ -7,25 +7,23 @@ namespace WMD.AI.Default
     {
         private const string ArgumentException_commandInputTypeNotRecognized = "Command input type not recognized.";
 
-        public static ICommandInputRetriever CreateICommandInputRetriever(Type commandInputType)
+        public static ICommandInputRetriever CreateICommandInputRetriever(Type commandInputType) => commandInputType switch
         {
-            return commandInputType switch
-            {
-                _ when commandInputType == typeof(AttackPlayerInput) => new AttackPlayerInputRetriever(),
-                _ when commandInputType == typeof(BuildSecretBaseInput) => new BuildSecretBaseInputRetriever(),
-                _ when commandInputType == typeof(DistributePropagandaInput) => new DistributePropagandaInputRetriever(),
-                _ when commandInputType == typeof(HireHenchmenInput) => new HireHenchmenInputRetriever(),
-                _ when commandInputType == typeof(LaunchNukesInput) => new LaunchNukesInputRetriever(),
-                _ when commandInputType == typeof(ManufactureNukesInput) => new ManufactureNukesInputRetriever(),
-                _ when commandInputType == typeof(PurchaseUnclaimedLandInput) => new PurchaseUnclaimedLandInputRetriever(),
-                _ when commandInputType == typeof(ResearchNukesInput) => new ResearchNukesInputRetriever(),
-                _ when commandInputType == typeof(ResignInput) => new ResignInputRetriever(),
-                _ when commandInputType == typeof(SellLandInput) => new SellLandInputRetriever(),
-                _ when commandInputType == typeof(SkipTurnInput) => new SkipTurnInputRetriever(),
-                _ when commandInputType == typeof(StealMoneyInput) => new StealMoneyInputRetriever(),
-                _ when commandInputType == typeof(UpgradeSecretBaseInput) => new UpgradeSecretBaseInputRetriever(),
-                _ => throw new ArgumentException(ArgumentException_commandInputTypeNotRecognized, nameof(commandInputType))
-            };
-        }
+            _ when commandInputType == typeof(AttackPlayerInput) => new AttackPlayerInputRetriever(),
+            _ when commandInputType == typeof(BuildSecretBaseInput) => new BuildSecretBaseInputRetriever(),
+            _ when commandInputType == typeof(ChangeDailyWageInput) => new ChangeDailyWageInputRetriever(),
+            _ when commandInputType == typeof(DistributePropagandaInput) => new DistributePropagandaInputRetriever(),
+            _ when commandInputType == typeof(HireHenchmenInput) => new HireHenchmenInputRetriever(),
+            _ when commandInputType == typeof(LaunchNukesInput) => new LaunchNukesInputRetriever(),
+            _ when commandInputType == typeof(ManufactureNukesInput) => new ManufactureNukesInputRetriever(),
+            _ when commandInputType == typeof(PurchaseUnclaimedLandInput) => new PurchaseUnclaimedLandInputRetriever(),
+            _ when commandInputType == typeof(ResearchNukesInput) => new ResearchNukesInputRetriever(),
+            _ when commandInputType == typeof(ResignInput) => new ResignInputRetriever(),
+            _ when commandInputType == typeof(SellLandInput) => new SellLandInputRetriever(),
+            _ when commandInputType == typeof(SkipTurnInput) => new SkipTurnInputRetriever(),
+            _ when commandInputType == typeof(StealMoneyInput) => new StealMoneyInputRetriever(),
+            _ when commandInputType == typeof(UpgradeSecretBaseInput) => new UpgradeSecretBaseInputRetriever(),
+            _ => throw new ArgumentException(ArgumentException_commandInputTypeNotRecognized, nameof(commandInputType))
+        };
     }
 }
