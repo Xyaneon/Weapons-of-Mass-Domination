c7bb9c895d1a55351a1de84e408df566453f6312
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-25 10:05:00 -0400

    Use expression body for CommandInputRetrieverFactory and CommandResultPrinterFactory

--- src/wmd-console/UI/Commands/CommandResultPrinterFactory.cs
+++ src/wmd-console/UI/Commands/CommandResultPrinterFactory.cs
@@ -7,26 +7,23 @@ namespace WMD.Console.UI.Commands
     {
         private const string ArgumentException_commandResultTypeNotRecognized = "Command result type not recognized.";
 
-        public static ICommandResultPrinter CreateICommandResultPrinter(Type commandResultType)
+        public static ICommandResultPrinter CreateICommandResultPrinter(Type commandResultType) => commandResultType switch
         {
-            return commandResultType switch
-            {
-                _ when commandResultType == typeof(AttackPlayerResult) => new AttackPlayerResultPrinter(),
-                _ when commandResultType == typeof(BuildSecretBaseResult) => new BuildSecretBaseResultPrinter(),
-                _ when commandResultType == typeof(ChangeDailyWageResult) => new ChangeDailyWageResultPrinter(),
-                _ when commandResultType == typeof(DistributePropagandaResult) => new DistributePropagandaResultPrinter(),
-                _ when commandResultType == typeof(HireHenchmenResult) => new HireHenchmenResultPrinter(),
-                _ when commandResultType == typeof(LaunchNukesResult) => new LaunchNukesResultPrinter(),
-                _ when commandResultType == typeof(ManufactureNukesResult) => new ManufactureNukesResultPrinter(),
-                _ when commandResultType == typeof(PurchaseUnclaimedLandResult) => new PurchaseUnclaimedLandResultPrinter(),
-                _ when commandResultType == typeof(ResearchNukesResult) => new ResearchNukesResultPrinter(),
-                _ when commandResultType == typeof(ResignResult) => new ResignResultPrinter(),
-                _ when commandResultType == typeof(SellLandResult) => new SellLandResultPrinter(),
-                _ when commandResultType == typeof(SkipTurnResult) => new SkipTurnResultPrinter(),
-                _ when commandResultType == typeof(StealMoneyResult) => new StealMoneyResultPrinter(),
-                _ when commandResultType == typeof(UpgradeSecretBaseResult) => new UpgradeSecretBaseResultPrinter(),
-                _ => throw new ArgumentException(ArgumentException_commandResultTypeNotRecognized, nameof(commandResultType))
-            };
-        }
+            _ when commandResultType == typeof(AttackPlayerResult) => new AttackPlayerResultPrinter(),
+            _ when commandResultType == typeof(BuildSecretBaseResult) => new BuildSecretBaseResultPrinter(),
+            _ when commandResultType == typeof(ChangeDailyWageResult) => new ChangeDailyWageResultPrinter(),
+            _ when commandResultType == typeof(DistributePropagandaResult) => new DistributePropagandaResultPrinter(),
+            _ when commandResultType == typeof(HireHenchmenResult) => new HireHenchmenResultPrinter(),
+            _ when commandResultType == typeof(LaunchNukesResult) => new LaunchNukesResultPrinter(),
+            _ when commandResultType == typeof(ManufactureNukesResult) => new ManufactureNukesResultPrinter(),
+            _ when commandResultType == typeof(PurchaseUnclaimedLandResult) => new PurchaseUnclaimedLandResultPrinter(),
+            _ when commandResultType == typeof(ResearchNukesResult) => new ResearchNukesResultPrinter(),
+            _ when commandResultType == typeof(ResignResult) => new ResignResultPrinter(),
+            _ when commandResultType == typeof(SellLandResult) => new SellLandResultPrinter(),
+            _ when commandResultType == typeof(SkipTurnResult) => new SkipTurnResultPrinter(),
+            _ when commandResultType == typeof(StealMoneyResult) => new StealMoneyResultPrinter(),
+            _ when commandResultType == typeof(UpgradeSecretBaseResult) => new UpgradeSecretBaseResultPrinter(),
+            _ => throw new ArgumentException(ArgumentException_commandResultTypeNotRecognized, nameof(commandResultType))
+        };
     }
 }
