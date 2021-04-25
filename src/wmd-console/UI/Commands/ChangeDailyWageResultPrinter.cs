ffc509ecff5bc6ac61d91f19c3ad17f2c81e2d66
Christopher Kyle Horton <christhehorton@gmail.com>
2021-04-25 09:59:45 -0400

    Add ChangeDailyWageResultPrinter class

---
+++ src/wmd-console/UI/Commands/ChangeDailyWageResultPrinter.cs
@@ -0,0 +1,20 @@
+﻿using WMD.Game.Commands;
+
+namespace WMD.Console.UI.Commands
+{
+    class ChangeDailyWageResultPrinter : CommandResultPrinter<ChangeDailyWageResult>
+    {
+        private const string PrintFormatString = "{0} changed their henchmen's daily pay rate from {1:C} to {2:C}.";
+
+        public override void PrintCommandResult(ChangeDailyWageResult result)
+        {
+            string formattedString = string.Format(
+                PrintFormatString,
+                RetrievePlayerWhoActed(result).Identification.Name,
+                result.OldDailyWage,
+                result.NewDailyWage
+            );
+            System.Console.WriteLine(formattedString);
+        }
+    }
+}
