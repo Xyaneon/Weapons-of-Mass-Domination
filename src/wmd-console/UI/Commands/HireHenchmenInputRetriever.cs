f2f8fbfc94755b14918d745fd9a89adb05f37e96
Christopher Kyle Horton <christhehorton@gmail.com>
2021-05-02 09:35:30 -0400

    Present upper limit on hiring in input retriever

--- src/wmd-console/UI/Commands/HireHenchmenInputRetriever.cs
+++ src/wmd-console/UI/Commands/HireHenchmenInputRetriever.cs
@@ -1,4 +1,5 @@
-﻿using WMD.Console.Miscellaneous;
+﻿using System;
+using WMD.Console.Miscellaneous;
 using WMD.Console.UI.Core;
 using WMD.Game.Commands;
 using WMD.Game.State.Data;
@@ -8,20 +9,30 @@ namespace WMD.Console.UI.Commands
     class HireHenchmenInputRetriever : ICommandInputRetriever
     {
         private const string ConfirmationPromptFormatString = "You will be looking to fill {0:N0} positions. Continue?";
-        private const string PositionsToOfferPrompt = "Please enter how many open positions you would like to offer";
+        private const string PositionsToOfferPromptFormatString = "Please enter how many open positions you would like to offer (between 1 and {0:N0})";
 
         public CommandInput? GetCommandInput(GameState gameState)
         {
-            var allowedPositionsToOffer = new LongRange(0, long.MaxValue);
-            var openPositionsToOffer = UserInput.GetLong(PositionsToOfferPrompt, allowedPositionsToOffer);
+            long maximumAllowedPositions = CalculateMaximumAllowedPositions(gameState);
+            if (maximumAllowedPositions == 0)
+            {
+                return null;
+            }
+
+            var allowedPositionsToOffer = new LongRange(0, maximumAllowedPositions);
+            var openPositionsToOffer = UserInput.GetLong(string.Format(PositionsToOfferPromptFormatString, maximumAllowedPositions), allowedPositionsToOffer);
             if (openPositionsToOffer <= 0)
             {
                 PrintingUtility.PrintNoPositionsToOffer();
                 return null;
             }
+
             return UserInput.GetConfirmation(string.Format(ConfirmationPromptFormatString, openPositionsToOffer))
                 ? new HireHenchmenInput() with { OpenPositionsOffered = openPositionsToOffer }
                 : null;
         }
+
+        private static long CalculateMaximumAllowedPositions(GameState gameState) =>
+            (long)Math.Floor(gameState.CurrentPlayer.State.Money / gameState.CurrentPlayer.State.WorkforceState.DailyPayRate);
     }
 }
