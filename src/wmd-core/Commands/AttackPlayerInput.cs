4ddb033e2d34b6b20dec588de3ac241aa35cc199
Christopher Kyle Horton <christhehorton@gmail.com>
2021-05-15 15:47:46 -0400

    Add AttackPlayerInput.NumberOfAttackingHenchmen property

--- src/wmd-core/Commands/AttackPlayerInput.cs
+++ src/wmd-core/Commands/AttackPlayerInput.cs
@@ -7,6 +7,28 @@ namespace WMD.Game.Commands
     /// </summary>
     public record AttackPlayerInput : CommandInput
     {
+        private const string ArgumentOutOfRangeException_TargetPlayerIndex_LessThanZero = "The target player index cannot be less than zero.";
+        private const string ArgumentOutOfRangeException_NumberOfAttackingHenchmen_LessThanOne = "The number of attacking henchmen cannot be less than one.";
+
+        /// <summary>
+        /// Gets or initializes the number of henchmen used to carry out the attack.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// The provided value is less than one.
+        /// </exception>
+        public long NumberOfAttackingHenchmen
+        {
+            get => _numberOfAttackingHenchmen;
+            init
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentOutOfRangeException(nameof(value), value, ArgumentOutOfRangeException_NumberOfAttackingHenchmen_LessThanOne);
+                }
+                _numberOfAttackingHenchmen = value;
+            }
+        }
+
         /// <summary>
         /// Gets or initializes the index of the player being attacked.
         /// </summary>
@@ -20,12 +42,13 @@ namespace WMD.Game.Commands
             {
                 if (value < 0)
                 {
-                    throw new ArgumentOutOfRangeException(nameof(value), value, "The target player index cannot be less than zero.");
+                    throw new ArgumentOutOfRangeException(nameof(value), value, ArgumentOutOfRangeException_TargetPlayerIndex_LessThanZero);
                 }
                 _targetPlayerIndex = value;
             }
         }
 
+        private long _numberOfAttackingHenchmen;
         private int _targetPlayerIndex;
     }
 }
