30e6c12ff5a477449ae15f10ea091c3e740828c9
Christopher Kyle Horton <christhehorton@gmail.com>
2021-06-19 14:52:45 -0400

    Add LandAreaCalculator.ClampLandAreaChangeAmount() method

--- src/wmd-core/State/Utility/LandAreaCalculator.cs
+++ src/wmd-core/State/Utility/LandAreaCalculator.cs
@@ -32,5 +32,23 @@ namespace WMD.Game.State.Utility
         /// <remarks>This method does not take into account the actual amount of remaining land area available for purchase.</remarks>
         public static decimal CalculateTotalPurchasePrice([DisallowNull] GameState gameState, int areaToPurchase) =>
             gameState.UnclaimedLandPurchasePrice * areaToPurchase;
+
+        /// <summary>
+        /// Takes a potential change in land area owned by a player and returns an amount which would stay within the allowed bounds.
+        /// </summary>
+        /// <param name="gameState">The current <see cref="GameState"/>.</param>
+        /// <param name="playerIndex">The index of the player for which the change is taking place.</param>
+        /// <param name="potentialAreaChange">The amount by which the player's owned land area could change if there was no limitation.</param>
+        /// <returns>
+        /// <paramref name="potentialAreaChange"/> if the resulting land area change for the player would result in a value between 0 and their
+        /// remaining land area if the change is negative, or between <paramref name="potentialAreaChange"/> and the total remaining unclaimed
+        /// land area if the change is positive; otherwise, 0.
+        /// </returns>
+        public static int ClampLandAreaChangeAmount([DisallowNull] GameState gameState, int playerIndex, int potentialAreaChange) => potentialAreaChange switch
+        {
+            _ when potentialAreaChange < 0 => Math.Max(potentialAreaChange, Math.Min(gameState.Players[playerIndex].State.Land, 0)),
+            _ when potentialAreaChange > 0 => Math.Min(potentialAreaChange, gameState.Planet.UnclaimedLandArea),
+            _ => 0,
+        };
     }
 }
