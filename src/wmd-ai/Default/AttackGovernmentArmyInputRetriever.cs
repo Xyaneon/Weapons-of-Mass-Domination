caea11b32036e326efecc58a0751cc59b420efe4
Christopher Kyle Horton <christhehorton@gmail.com>
2021-06-26 13:31:36 -0400

    Add AttackGovernmentArmyInputRetriever class

---
+++ src/wmd-ai/Default/AttackGovernmentArmyInputRetriever.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using WMD.Game.Commands;
+using WMD.Game.State.Data;
+
+namespace WMD.AI.Default
+{
+    internal sealed class AttackGovernmentArmyInputRetriever : ICommandInputRetriever
+    {
+        public CommandInput? GetCommandInput(GameState gameState)
+        {
+            if (!HasHenchmenToAttackWith(gameState))
+            {
+                return null;
+            }
+
+            long henchmenToAttackWith = CalculateHenchmenToAttackWith(gameState);
+
+            return new AttackGovernmentArmyInput() { NumberOfAttackingHenchmen = henchmenToAttackWith };
+        }
+
+        private static long CalculateHenchmenToAttackWith(GameState gameState) =>
+            Math.Max(gameState.CurrentPlayer.State.WorkforceState.NumberOfHenchmen / 2, 1);
+
+        private static bool HasHenchmenToAttackWith(GameState gameState) => gameState.CurrentPlayer.State.WorkforceState.NumberOfHenchmen > 0;
+    }
+}
