7c192b5fd2b892ee25ea9526d54da62ce4233069
Christopher Kyle Horton <christhehorton@gmail.com>
2022-12-26 20:17:54 -0500

    Fix failing test case with check on how much land is actually available

--- src/wmd-core/State/Utility/LandAreaCalculator.cs
+++ src/wmd-core/State/Utility/LandAreaCalculator.cs
@@ -14,14 +14,19 @@ namespace WMD.Game.State.Utility
         /// </summary>
         /// <param name="gameState">The current <see cref="GameState"/>.</param>
         /// <returns>The maximum amount of land area the current player could purchase with their funds, in square kilometers.</returns>
-        /// <remarks>This method does not take into account the actual amount of remaining land area available for purchase.</remarks>
+        /// <remarks>This method takes into account the actual amount of remaining land area available for purchase.</remarks>
         public static int CalculateMaximumLandAreaCurrentPlayerCouldPurchase([DisallowNull] GameState gameState)
         {
             decimal availableFunds = gameState.CurrentPlayer.State.Money;
             decimal pricePerSquareKilometer = gameState.UnclaimedLandPurchasePrice;
-            return (int)Math.Floor(availableFunds / pricePerSquareKilometer);
-        }
+            int totalUnclaimedLandArea = gameState.Planet.UnclaimedLandArea;
 
+            decimal theoreticalMaximumLandArea = Math.Floor(availableFunds / pricePerSquareKilometer);
+
+            return theoreticalMaximumLandArea > totalUnclaimedLandArea
+                ? totalUnclaimedLandArea
+                : (int)theoreticalMaximumLandArea;
+        }
 
         /// <summary>
         /// Calculates the total purchase price for the given area of unclaimed land.
