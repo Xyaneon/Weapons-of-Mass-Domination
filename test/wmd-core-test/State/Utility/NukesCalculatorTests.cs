79b33a1258356d9e6513fd0e45e7a071be2968b0
Christopher Kyle Horton <christhehorton@gmail.com>
2022-12-26 21:42:21 -0500

    Add failing test case when player already has nukes

--- test/wmd-core-test/State/Utility/NukesCalculatorTests.cs
+++ test/wmd-core-test/State/Utility/NukesCalculatorTests.cs
@@ -15,10 +15,14 @@ namespace WMD.Game.Test.State.Utility
         [DynamicData(nameof(CalculateMaximumNumberOfNukesCurrentPlayerCouldManufactureData))]
         public void CalculateMaximumNumberOfNukesCurrentPlayerCouldManufacture_ShouldReturnExpectedAmountForGameState(
             decimal playerMoney,
+            int initialNukes,
             int expectedNukesPlayerCouldPurchase
         )
         {
-            var playerState = new PlayerState() with { Money = playerMoney };
+            var playerState = new PlayerState() with {
+                Money = playerMoney,
+                Nukes = initialNukes
+            };
             var player = new Player(
                 new PlayerIdentification("Test player", PlayerColor.Red, true)
             ) with { State = playerState };
@@ -27,15 +31,22 @@ namespace WMD.Game.Test.State.Utility
 
             int actual = NukesCalculator.CalculateMaximumNumberOfNukesCurrentPlayerCouldManufacture(gameState);
 
-            Assert.AreEqual(expectedNukesPlayerCouldPurchase, actual, "Money: {0}", playerMoney);
+            Assert.AreEqual(
+                expectedNukesPlayerCouldPurchase,
+                actual,
+                "Money: <{0}>. Initial nukes: <{1}>.",
+                playerMoney,
+                initialNukes
+            );
         }
 
         public static IEnumerable<Object[]> CalculateMaximumNumberOfNukesCurrentPlayerCouldManufactureData =>
             new [] {
-                new Object[] { 0m, 0 },
-                new Object[] { 1000m, 1 },
-                new Object[] { 2000m, 2 },
-                new Object[] { 2338310951653.58m, Int32.MaxValue },
+                new Object[] { 0m, 0, 0 },
+                new Object[] { 1000m, 0, 1 },
+                new Object[] { 2000m, 0, 2 },
+                new Object[] { 2338310951653.58m, 0, Int32.MaxValue },
+                new Object[] { 2338310951653.58m, 2, Int32.MaxValue - 2 },
             };
     }
 }
