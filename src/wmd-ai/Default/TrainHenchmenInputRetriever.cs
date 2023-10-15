d92532bfe2012bfe3672cba4d39b8ddfc08bdd50
Christopher Kyle Horton <christhehorton@gmail.com>
2023-10-15 18:16:05 -0400

    Allow computer players to train thieves

--- src/wmd-ai/Default/TrainHenchmenInputRetriever.cs
+++ src/wmd-ai/Default/TrainHenchmenInputRetriever.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using WMD.Game.Commands;
 using WMD.Game.State.Data;
 using WMD.Game.State.Data.Henchmen;
@@ -12,17 +14,20 @@ internal sealed class TrainHenchmenInputRetriever : ICommandInputRetriever
     public CommandInput? GetCommandInput(GameState gameState) => new TrainHenchmenInput
     {
         NumberToTrain = CalculateNumberOfTrainees(gameState),
-        Specialization = DetermineTrainingSpecialization(gameState),
+        Specialization = DetermineTrainingSpecialization(),
     };
 
     private static long CalculateNumberOfTrainees(GameState gameState) =>
         _random.NextInt64(1, gameState.CurrentPlayer.State.WorkforceState.GenericHenchmenCount);
 
-    private static Specialization DetermineTrainingSpecialization(GameState gameState)
-    {
-        // TODO: Make this return a random value other than Untrained once more specializations are available.
-        return Specialization.Soldier;
-    }
+    private static Specialization DetermineTrainingSpecialization() =>
+        new List<Specialization>
+        {
+            Specialization.Soldier,
+            Specialization.Thief
+        }
+        .OrderBy(_ => _random.Next())
+        .First();
 
     private static readonly Random _random;
 }
